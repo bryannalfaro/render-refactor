bff3626de4d23583ce20567649ec087a119739b3
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-08-31 15:06:23 -0600

    refactoring files

--- Funciones/utilities.py
+++ Funciones/utilities.py
@@ -1,5 +1,5 @@
 from Funciones.math import *
-
+import random
 class V3(object):
     def __init__(self, x, y, z=None):
         self.x = x
@@ -63,3 +63,103 @@ def bbox(A,B,C):
     ys = [A.y, B.y, C.y,]
     ys.sort()
     return xs[0],xs[-1],ys[0],ys[-1]
+
+def transform(v, translate, scale):
+        return V3(
+            round(((v[0]+translate[0])*scale[0])),
+            round(((v[1]+translate[1])*scale[1])),
+            round(((v[2]+translate[2])*scale[2]))
+        )
+
+def shader(self, x,y):
+        centerx,centery = 500,353
+        centerx2,centery2 = 478,360
+        circle1 = (x-centerx)**2+(y-centery)**2
+        circle2 = (x-centerx2)**2+(y-centery2)**2
+
+        #Colors of the image
+        palette = [(171,42,120),(8,4,6),(246,130,199),
+        (111,17,76),(92,15,58),(225,76,167),(238,94,183),
+        (206,90,155),(76,76,76)]
+
+        if y >= 100 and y < 165:
+            if x > (300) and x< (460+random.randint(0,10)):
+                return palette[3][2],palette[3][1],palette[3][0]
+            return palette[4][2],palette[4][1],palette[4][0]
+
+        if y >= 165 and y < (185+random.randint(0,12)):
+            if x > (300) and x< (460+random.randint(0,20)):
+                return palette[3][2],palette[3][1],palette[3][0]
+            else:
+                return palette[4][2],palette[4][1],palette[4][0]
+
+        if y >= (185+random.randint(0,12)) and y < (220+random.randint(0,12)):
+            return palette[0][2],palette[0][1],palette[0][0]
+
+        if y>=220 and y < (250+random.randint(0,12)):
+            if y > 220 and y < 230:
+                if x > 300 and x< (480+random.randint(0,18)):
+                    return palette[6][2],palette[6][1],palette[6][0]
+            if y > 230 and y < 235:
+                if x > 300 and x< (460+random.randint(0,18)):
+                    return palette[6][2],palette[6][1],palette[6][0]
+                if x > 460 and x< (520+random.randint(0,18)):
+                    return palette[2][2],palette[2][1],palette[2][0]
+            if y >= (235+random.randint(0,18)) and y <=(245+random.randint(0,18)):
+                if x > (420+random.randint(0,15)) and x< (520+random.randint(0,18)):
+                    return palette[2][2],palette[2][1],palette[2][0]
+            if y > 245 and y < 250:
+                if x > 300 and x< (480+random.randint(0,18)):
+                    return palette[6][2],palette[6][1],palette[6][0]
+                if x > 460 and x< (520+random.randint(0,18)):
+                    return palette[2][2],palette[2][1],palette[2][0]
+            if x > 300 and x< (430+random.randint(0,12)):
+                return palette[6][2],palette[6][1],palette[6][0]
+            return palette[7][2],palette[7][1],palette[7][0]
+
+
+        if y >= 250 and y < (270+random.randint(0,12)):
+            return palette[5][2],palette[5][1],palette[5][0]
+
+        if y>=270 and y < (280+random.randint(0,16)):
+            if x > 300 and x< (480+random.randint(0,18)):
+                return palette[6][2],palette[6][1],palette[6][0]
+            else:
+                return palette[5][2],palette[5][1],palette[5][0]
+
+        if y>=280 and y < (310+random.randint(0,30)):
+            return palette[5][2],palette[5][1],palette[5][0]
+
+        if y >= 310 and y < (340+random.randint(0,10)):
+            return palette[7][2],palette[7][1],palette[7][0]
+
+        if y >= 340 and y < (375):
+            return palette[0][2],palette[0][1],palette[0][0]
+
+        if y >=(375) and y < (395+random.randint(0,12)):
+            if circle1 < 40**2:
+                return palette[0][2],palette[0][1],palette[0][0]
+            if circle2 < 30**2:
+
+                return palette[0][2],palette[0][1],palette[0][0]
+            if y > 375 and y <382:
+                if x > (390+random.randint(0,15)) and x < (450+random.randint(0,14)):
+                    return palette[2][2],palette[2][1],palette[2][0]
+                if x > (480+random.randint(0,15)) and x < (580+random.randint(0,14)):
+                    return palette[2][2],palette[2][1],palette[2][0]
+            if y > 380 and y<395:
+
+                if x > 300 and x < (430+random.randint(0,50)):
+                    return palette[6][2],palette[6][1],palette[6][0]
+            if x > 300 and x < (430+random.randint(0,15)):
+                return palette[6][2],palette[6][1],palette[6][0]
+            return palette[7][2],palette[7][1],palette[7][0]
+
+        if y>=(395) and y < (410+random.randint(0,12)):
+            return palette[5][2],palette[5][1],palette[5][0]
+
+        if y >= 410 and y <(430+random.randint(0,4)):
+            return palette[0][2],palette[0][1],palette[0][0]
+
+        else:
+            return palette[3][2],palette[3][1],palette[3][0]
--- gl.py
+++ gl.py
@@ -142,21 +142,14 @@ class Renderer(object):
             for pointf in points:
                 self.point(*pointf)
 
-    def transform(self, v, translate, scale):
-        return V3(
-            round(((v[0]+translate[0])*scale[0])),
-            round(((v[1]+translate[1])*scale[1])),
-            round(((v[2]+translate[2])*scale[2]))
-        )
 
     def load(self, filename, movement, scale):
         model = Obj(filename)
 
-
         vertex_buffer_object = []
         for face in (model.faces):
             for v in range(len(face)):
-                vertex = self.transform(V3(*model.vertices[face[v][0]-1]), movement,scale)
+                vertex = transform(V3(*model.vertices[face[v][0]-1]), movement,scale)
                 vertex_buffer_object.append(vertex)
             if self.texture:
                 for v in range(len(face)):
@@ -181,7 +174,6 @@ class Renderer(object):
                     self.square()
             except StopIteration:
                 print('Done')
-
 
     def fill(self):
         bandera = False
@@ -249,7 +241,6 @@ class Renderer(object):
                     self.squareDraw(A,B,C,tA,tB,tC)
                 else:
                     self.squareDraw(A,B,C,tA,tB,tC)
-
 
     def squareDraw(self,A,B,C,tA,tB,tC):
             xmin,xmax,ymin,ymax = bbox(A,B,C)
@@ -337,96 +328,5 @@ class Renderer(object):
                     except:
                         pass
 
-    def shader(self, x,y):
-        centerx,centery = 500,353
-        centerx2,centery2 = 478,360
-        circle1 = (x-centerx)**2+(y-centery)**2
-        circle2 = (x-centerx2)**2+(y-centery2)**2
 
-        #Colors of the image
-        palette = [(171,42,120),(8,4,6),(246,130,199),
-        (111,17,76),(92,15,58),(225,76,167),(238,94,183),
-        (206,90,155),(76,76,76)]
-
-        if y >= 100 and y < 165:
-            if x > (300) and x< (460+random.randint(0,10)):
-                return palette[3][2],palette[3][1],palette[3][0]
-            return palette[4][2],palette[4][1],palette[4][0]
-
-        if y >= 165 and y < (185+random.randint(0,12)):
-            if x > (300) and x< (460+random.randint(0,20)):
-                return palette[3][2],palette[3][1],palette[3][0]
-            else:
-                return palette[4][2],palette[4][1],palette[4][0]
-
-        if y >= (185+random.randint(0,12)) and y < (220+random.randint(0,12)):
-            return palette[0][2],palette[0][1],palette[0][0]
-
-        if y>=220 and y < (250+random.randint(0,12)):
-            if y > 220 and y < 230:
-                if x > 300 and x< (480+random.randint(0,18)):
-                    return palette[6][2],palette[6][1],palette[6][0]
-            if y > 230 and y < 235:
-                if x > 300 and x< (460+random.randint(0,18)):
-                    return palette[6][2],palette[6][1],palette[6][0]
-                if x > 460 and x< (520+random.randint(0,18)):
-                    return palette[2][2],palette[2][1],palette[2][0]
-            if y >= (235+random.randint(0,18)) and y <=(245+random.randint(0,18)):
-                if x > (420+random.randint(0,15)) and x< (520+random.randint(0,18)):
-                    return palette[2][2],palette[2][1],palette[2][0]
-            if y > 245 and y < 250:
-                if x > 300 and x< (480+random.randint(0,18)):
-                    return palette[6][2],palette[6][1],palette[6][0]
-                if x > 460 and x< (520+random.randint(0,18)):
-                    return palette[2][2],palette[2][1],palette[2][0]
-            if x > 300 and x< (430+random.randint(0,12)):
-                return palette[6][2],palette[6][1],palette[6][0]
-            return palette[7][2],palette[7][1],palette[7][0]
-
-
-        if y >= 250 and y < (270+random.randint(0,12)):
-            return palette[5][2],palette[5][1],palette[5][0]
-
-        if y>=270 and y < (280+random.randint(0,16)):
-            if x > 300 and x< (480+random.randint(0,18)):
-                return palette[6][2],palette[6][1],palette[6][0]
-            else:
-                return palette[5][2],palette[5][1],palette[5][0]
-
-        if y>=280 and y < (310+random.randint(0,30)):
-            return palette[5][2],palette[5][1],palette[5][0]
-
-        if y >= 310 and y < (340+random.randint(0,10)):
-            return palette[7][2],palette[7][1],palette[7][0]
-
-        if y >= 340 and y < (375):
-            return palette[0][2],palette[0][1],palette[0][0]
-
-        if y >=(375) and y < (395+random.randint(0,12)):
-            if circle1 < 40**2:
-                return palette[0][2],palette[0][1],palette[0][0]
-            if circle2 < 30**2:
-
-                return palette[0][2],palette[0][1],palette[0][0]
-            if y > 375 and y <382:
-                if x > (390+random.randint(0,15)) and x < (450+random.randint(0,14)):
-                    return palette[2][2],palette[2][1],palette[2][0]
-                if x > (480+random.randint(0,15)) and x < (580+random.randint(0,14)):
-                    return palette[2][2],palette[2][1],palette[2][0]
-            if y > 380 and y<395:
-
-                if x > 300 and x < (430+random.randint(0,50)):
-                    return palette[6][2],palette[6][1],palette[6][0]
-            if x > 300 and x < (430+random.randint(0,15)):
-                return palette[6][2],palette[6][1],palette[6][0]
-            return palette[7][2],palette[7][1],palette[7][0]
-
-        if y>=(395) and y < (410+random.randint(0,12)):
-            return palette[5][2],palette[5][1],palette[5][0]
-
-        if y >= 410 and y <(430+random.randint(0,4)):
-            return palette[0][2],palette[0][1],palette[0][0]
-
-        else:
-            return palette[3][2],palette[3][1],palette[3][0]
 
