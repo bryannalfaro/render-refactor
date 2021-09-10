2c49536ca300d2373790aa35d9c12602e9ea09e9
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-09-10 11:52:34 -0600

    feat: organizando modelos

--- Motor.py
+++ Motor.py
@@ -30,38 +30,40 @@ r.active_shader = gourad
 
 t= Texture('./Texturas/trump.bmp')
 r.texture = t
-r.load('./newModels/trump.obj',(0,0,0),(0.4,0.4,0.4),(0,0,0))
+r.load('./newModels/trump.obj',(0.02,0.2,0.8),(0.2,0.2,0.2),(0,0,0))
 r.draw_arrays('TRIANGLES')
 
-t = Texture('./Texturas/wine.bmp')
+'''t = Texture('./Texturas/wine.bmp')
 r.texture = t
 r.load('./newModels/wine.obj',(0,0,0),(0.009,0.03,0.03),(pi/2,pi,pi))
-r.draw_arrays('SQUARE')
+r.draw_arrays('SQUARE')'''
 
 '''t= Texture('./Texturas/Omnitrix.bmp')
 r.texture = t
 r.load('./newModels/ovni.obj',(0,0,0),(0.04,0.04,0.04),(0,pi/8,0))
-r.draw_arrays('TRIANGLES')
+r.draw_arrays('TRIANGLES')'''
 
 
 t = Texture('./Texturas/Horse_v01.bmp')
 r.texture = t
-r.load('./newModels/horse1.obj',(0,0,0),(0.0002,0.0002,0.0002),(0,0,pi/8))
+#-pi/2
+#2*pi
+r.load('./newModels/horse1.obj',(0,0,0.7),(0.0003,0.0003,0.0003),(-pi/2,2*pi,2*pi))
 r.draw_arrays('SQUARE')
 
 t = Texture('./Texturas/doff.bmp')
 r.texture = t
-r.load('./newModels/dog11.obj',(0.2,0,0),(0.005,0.005,0.005),(0,0,0))
+r.load('./newModels/dog11.obj',(0.3,0,0.7),(0.005,0.005,0.005),(-pi/2,2*pi,-pi/2))
 r.draw_arrays('SQUARE')
 
 t = Texture('./Texturas/wine.bmp')
 r.texture = t
-r.load('./newModels/wine.obj',(0,0,0),(0.009,0.03,0.03),(pi/2,pi,pi))
+r.load('./newModels/wine.obj',(0.2,0.4,0.5),(0.004,0.004,0.009),(pi/2,pi,pi))
 r.draw_arrays('SQUARE')
 
 t = Texture('./Texturas/textureWolf.bmp')
 r.texture = t
-r.load('./newModels/wolf.obj',(0,0,0),(0.006,0.006,0.006),(0,0,0))
-r.draw_arrays('TRIANGLES')'''
+r.load('./newModels/wolf.obj',(-0.7,0,0),(0.003,0.003,0.003),(0,pi/4,0))
+r.draw_arrays('TRIANGLES')
 r.glFinish("./salidas/clear.bmp")
 
